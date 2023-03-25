d2f637530eb73d4d0944e3273f4f816e192dcf24
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-26 00:02:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace homework_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a number=> "); int.TryParse(Console.ReadLine(), out int number);
+                int factorial = 1;
+                int i = 1;
+                do
+                {
+                    factorial *= i; i++;
+                }
+                while (i <= number);
+                Console.WriteLine($"Factorial of {number} is {factorial}.");
+            }
+        }
+    }
+}
